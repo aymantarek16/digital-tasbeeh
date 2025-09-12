0a9644da9779d3a805383843de4e2b0c44823d39
aymantarek16 <aymantarekm16@gmail.com>
2025-09-12 21:23:40 +0300

    change favicon

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'السبحة الرقمية',
+        short_name: 'سبحة',
+        start_url: '.',
+        display: 'standalone',
+        background_color: '#16a34a',
+        theme_color: '#16a34a',
+        icons: [
+          { src: '/bg_sebha.png', sizes: '192x192', type: 'image/png' },
+          { src: '/bg_sebha.png', sizes: '512x512', type: 'image/png' }
+        ]
+      }
+    })
+  ]
 })
