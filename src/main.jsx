20f5872b25600e434b2ee90937d34d04a87130a9
aymantarek16 <aymantarekm16@gmail.com>
2025-09-12 21:14:13 +0300

    change favicon

--- src/main.jsx
+++ src/main.jsx
@@ -6,3 +6,13 @@ import "./index.css";
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
+
+// ✅ تسجيل الـ Service Worker
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(() => console.log("✅ Service Worker Registered"))
+      .catch((err) => console.error("❌ Service Worker Error:", err));
+  });
+}
